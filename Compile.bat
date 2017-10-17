8356e84afba03cf47ef932a28d345e04735ff29c
iceprincefounder <iceprincefounder@qq.com>
2017-10-17 21:22:49 +0800

    call Visual Studio command cl to build shader

--- Compile.bat
+++ Compile.bat
@@ -1,2 +1,4 @@
+call C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/vcvarsall.bat
+
 set ARNOLD_PATH=c:/path/to/arnold
-cl /LD simple_shader.cpp /I %ARNOLD_PATH%/include %ARNOLD_PATH%/lib/ai.lib /link /out:simple_shader.dll
+rem cl /LD simple_shader.cpp /I %ARNOLD_PATH%/include %ARNOLD_PATH%/lib/ai.lib /link /out:simple_shader.dll
