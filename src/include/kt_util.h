cedabdc0c1994460d1c960f6ea17aa058943cb38
iceprincefounder <iceprincefounder@qq.com>
2017-10-17 17:26:32 +0800

    add custom AOVs,finish baisc fucntions

--- src/include/kt_util.h
+++ src/include/kt_util.h
@@ -12,3 +12,41 @@
 //   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
+#pragma once
+#include <vector>
+#include <string>
+#include <math.h>
+#include <cassert>
+
+#ifndef REGISTER_AOVS_CUSTOM
+#define REGISTER_AOVS_CUSTOM \
+data->aovs_custom.clear(); \
+data->aovs_custom.push_back(params[p_aov_color_major].STR); \
+data->aovs_custom.push_back(params[p_aov_color_shadow].STR); \
+data->aovs_custom.push_back(params[p_aov_color_mask].STR); \
+data->aovs_custom.push_back(params[p_aov_color_extra].STR); \
+data->aovs_custom.push_back(params[p_aov_diffuse_color].STR); \
+data->aovs_custom.push_back(params[p_aov_specular_color].STR); \
+assert(data->aovs_custom.size() == 6 && "NUM_AOVs does not match size of aovs array!"); \
+for (size_t i=0; i < data->aovs_custom.size(); ++i) \
+	AiAOVRegister(data->aovs_custom[i].c_str(), AI_TYPE_RGB, AI_AOV_BLEND_OPACITY);
+#endif
+
+enum AovIndices
+{
+	k_color_major = 0,
+	k_color_shadow,
+	k_color_mask,
+	k_color_extra,
+	k_diffuse_color,
+	k_specular_color
+};
+
+
+struct ShaderData
+{
+    // AOV names
+    std::vector<std::string> aovs;
+    std::vector<std::string> aovs_rgba;
+    std::vector<std::string> aovs_custom;
+};
--- src/syToons.cpp
+++ src/syToons.cpp
@@ -16,6 +16,7 @@
 #include <cstring>
 
 #include <al_util.h>
+#include <kt_util.h>
 
 AI_SHADER_NODE_EXPORT_METHODS(syToonsMethods);
 
@@ -38,11 +39,19 @@ enum Params {
 	p_color_shadow,
 	p_color_mask,
 	p_color_extra,
-	p_Ka_color,
-	p_Kd_color,
-	p_Ks_color,
+	p_diffuse_color,
+	p_specular_color,
 	p_roughness,
-	p_specbalance
+	//p_specbalance //hide parameter specbalance until we need ray-tracing render
+	p_casting_light,
+	p_casting_texture,
+	
+	p_aov_color_major,
+	p_aov_color_shadow,
+	p_aov_color_mask,
+	p_aov_color_extra,
+	p_aov_diffuse_color,
+	p_aov_specular_color,
 };
 
 node_parameters
@@ -52,50 +61,75 @@ node_parameters
 	AiParameterRGB("color_shadow", 0.0f, 0.0f, 0.0f);
 	AiParameterFLT("color_mask", 0.0f);
 	AiParameterRGB("color_extra", 0.0f, 0.0f, 0.0f);
-	AiParameterRGB("Ka_color", 0.0f, 0.0f, 0.0f);
-	AiParameterRGB("Kd_color", 0.7f, 0.7f, 0.7f);
-	AiParameterRGB("Ks_color", 0.7f, 0.7f, 0.7f);
+	AiParameterRGB("diffuse_color", 0.7f, 0.7f, 0.7f);
+	AiParameterRGB("specular_color", 1.0f, 1.0f, 1.0f);
 	AiParameterFLT("roughness", 0.2f);
-	AiParameterFLT("specbalance", 0.1f);
+	//AiParameterFLT("specbalance", 0.1f); ////hide parameter specbalance until we need ray-tracing render
+	AiParameterBool("casting_light", true);
+	AiParameterBool("casting_texture", true);
+
+	AiParameterStr("aov_color_major", "color_major");
+	AiParameterStr("aov_color_shadow", "color_shadow");
+	AiParameterStr("aov_color_mask", "color_mask");
+	AiParameterStr("aov_color_extra", "color_extra");
+	AiParameterStr("aov_diffuse_color", "diffuse_color");
+	AiParameterStr("aov_specular_color", "specular_color");
 }
 
 node_initialize
 {
+	ShaderData* data = new ShaderData();
+	AiNodeSetLocalData(node, data);
 }
 
 node_update
 {
+	ShaderData* data = (ShaderData*)AiNodeGetLocalData(node);
+	// set up AOVs
+	REGISTER_AOVS_CUSTOM
 }
 
 node_finish
 {
+	if (AiNodeGetLocalData(node))
+	{
+		ShaderData* data = (ShaderData*)AiNodeGetLocalData(node);
+		AiNodeSetLocalData(node, NULL);
+		delete data;
+	}
 }
 
 shader_evaluate
 {
+	ShaderData* data = (ShaderData*)AiNodeGetLocalData(node);
 	// we provide two shading engine,traditional scanline and GI engine raytrace.
 	int shading_engine = AiShaderEvalParamInt(p_engine);
 	
 	AtColor color_major = AiShaderEvalParamRGB(p_color_major);
 	AtColor color_shadow = AiShaderEvalParamRGB(p_color_shadow);
-	float color_mask = AiShaderEvalParamFlt(p_color_mask);
+	AtColor color_mask = AiShaderEvalParamRGB(p_color_mask);
 	AtColor color_extra = AiShaderEvalParamRGB(p_color_extra);
-	//if (color_major != AI_RGB_BLACK)
-	//	AiAOVSetRGB(sg, data->aovs_custom[k_diffuse_color].c_str(), color_major);
+	bool casting_light = AiShaderEvalParamBool(p_casting_light);
+	bool casting_texture = AiShaderEvalParamBool(p_casting_texture);
 
+	// Set flat shader AOVs
+	AiAOVSetRGB(sg, data->aovs_custom[k_color_major].c_str(), color_major);
+	AiAOVSetRGB(sg, data->aovs_custom[k_color_shadow].c_str(), color_shadow);
+	AiAOVSetRGB(sg, data->aovs_custom[k_color_mask].c_str(), color_mask);
+	AiAOVSetRGB(sg, data->aovs_custom[k_color_extra].c_str(), color_extra);
 	// do shading
 	AtColor lighting_result = AI_RGB_BLACK;
 	switch (shading_engine)
 	{
 		case S_SCANLINE:
 		{
-			AtColor Ka = AiShaderEvalParamRGB(p_Ka_color);
-			AtColor Kd = AiShaderEvalParamRGB(p_Kd_color);
-			AtColor Ks = AiShaderEvalParamRGB(p_Ks_color);
+			AtColor Kd = AiShaderEvalParamRGB(p_diffuse_color);
+			AtColor Ks = AiShaderEvalParamRGB(p_specular_color);
 			float Wig = 0.28;
 			float roughness = 10 / AiShaderEvalParamFlt(p_roughness);
 			AiLightsPrepare(sg);
-			AtColor La = AI_RGB_BLACK; // initialize light accumulator to = 0
+			AtColor LaD = AI_RGB_BLACK; // initialize light accumulator to = 0
+			AtColor LaS = AI_RGB_BLACK; // initialize light accumulator to = 0
 			while (AiLightsGetSample(sg)) // loop over the lights
 			{
 				float LdotN = AiV3Dot(sg->Ld, sg->Nf);
@@ -104,21 +138,28 @@ shader_evaluate
 				float spec = AiV3Dot(sg->Nf, H); // N dot H
 				if (spec < 0) spec = 0;
 				// Lambertian diffuse
-				La += sg->Li * Wig * sg->we * LdotN * Kd;
+				LaD += sg->Li * Wig * sg->we * LdotN * Kd;
 				// Blinn-Phong specular
-				La += sg->Li * Wig * sg->we * pow(spec, roughness) * Ks;
+				LaS += sg->Li * Wig * sg->we * pow(spec, roughness) * Ks;
 			}
+
+			// add diffuse and specular into custom AOVs
+			if (LaD != AI_RGB_BLACK)
+				AiAOVSetRGB(sg, data->aovs_custom[k_diffuse_color].c_str(), LaD);
+			if (LaS != AI_RGB_BLACK)
+				AiAOVSetRGB(sg, data->aovs_custom[k_specular_color].c_str(), LaS);
+
 			// color = accumulated light + ambient
-			lighting_result = La + Ka;
+			lighting_result = LaD + LaS;
 			break;   	   			
 		}
 		case S_RAYTRACE:
 		{
 			// Kd (diffuse color), Ks (specular color), and roughness (scalar)
-			AtColor Kd = AiShaderEvalParamRGB(p_Kd_color);
-			AtColor Ks = AiShaderEvalParamRGB(p_Ks_color);
+			AtColor Kd = AiShaderEvalParamRGB(p_diffuse_color);
+			AtColor Ks = AiShaderEvalParamRGB(p_specular_color);
 			float roughness = AiShaderEvalParamFlt(p_roughness);
-			float specbalance = AiShaderEvalParamFlt(p_specbalance);
+			float specbalance = 0.1f; //hide parameter specbalance until we need ray-tracing render
 
 			// direct specular and diffuse accumulators, 
 			// and indirect diffuse and specular accumulators...
@@ -141,6 +182,12 @@ shader_evaluate
 			// indirect diffuse
 			IDd = AiOrenNayarIntegrate(&sg->Nf, sg, 0.0f) * (1-specbalance);
 
+			// add direct diffuse and direct specular into custom AOVs
+			if (Dda != AI_RGB_BLACK)
+				AiAOVSetRGB(sg, data->aovs_custom[k_diffuse_color].c_str(), Dda);
+			if (Dsa != AI_RGB_BLACK)
+				AiAOVSetRGB(sg, data->aovs_custom[k_specular_color].c_str(), Dsa);
+
 			// add up indirect and direct contributions
 			lighting_result = Kd * (Dda + IDd) + Ks * (Dsa + IDs);
 			break;		
@@ -152,6 +199,9 @@ shader_evaluate
 			}
 	}
 	//sg->out.RGB = color_major + color_secondary*color_shadow;
-	AtColor texture_result = lerp(color_shadow,color_major,color_mask) + color_extra;
-	sg->out.RGB = texture_result;
+	AtColor texture_result = lerp(color_shadow,color_major,color_mask.r) + color_extra;
+	if(casting_light)
+		sg->out.RGB = lighting_result;
+	if(casting_texture)
+		sg->out.RGB = texture_result;
 }
