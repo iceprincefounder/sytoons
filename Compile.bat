b821db1c70a79b1901b2c0edfa35a842f976b10e
iceprincefounder <iceprincefounder@qq.com>
2017-12-02 19:03:24 +0800

    new windows build environment

--- Compile.bat
+++ Compile.bat
@@ -1,5 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 set > environment.txt
-set ARNOLD_PATH="C:/Workspace/arnold-4.2.11.3-windows"
+set ARNOLD_PATH="D:/Workspace/arnold-4.2.11.3-windows"
 
 cl /LD src/syToons.cpp src/syRamp.cpp src/syRemap.cpp src/syOutline.cpp src/shader_loader.cpp /I %ARNOLD_PATH%/include /I src/include %ARNOLD_PATH%/lib/ai.lib /link /out:bin/sytoons.dll
